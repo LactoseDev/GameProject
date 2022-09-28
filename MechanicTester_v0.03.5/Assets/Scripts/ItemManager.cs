2f46221dd0dd567f2311faca3d5153d6dffa330a
PokerDott <quinnporter69@gmail.com>
2022-09-29 12:47:02 +1300

    Commit Seven
    Workshop, 29/09/22.

---
+++ MechanicTester_v0.03.5/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemManager : MonoBehaviour
+{
+    /*public int coinCount;
+
+    public TextMeshProUGUI coinCountText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinCountText.text = "" + coinCount;
+    }
+
+    public void HandleCollectible()
+    {
+        coinCount += collectibleValue;
+    }*/
+
+
+}
