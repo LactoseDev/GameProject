f60fc3782304ed8b07028e7b398f5be84ea1919f
EscherVI <113559975+EscherVI@users.noreply.github.com>
2022-11-01 14:31:13 +1300

    Added Player Hit Animation
    added the "Hit" animation to the player when they take damage

--- MechanicTester_v0.03.5/Assets/Scripts/Health/PlayerHealth.cs
+++ MechanicTester_v0.03.5/Assets/Scripts/Health/PlayerHealth.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PlayerHealth : Health
 {
     public GameObject checkpoint;
+    private Animator anim;
+
+    protected virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     public override void HandleDamage(int damageValue)
     {
         base.HandleDamage(damageValue);
+        anim.SetTrigger("hit");
+        Debug.Log("HitAnimation");
 
         // Death Logic
         if (currentHealth <= 0)
